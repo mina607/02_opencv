ee819a4e5e7f86b13d266baffa8c0f61ee06eb38
mina607 <choimina030607@gmail.com>
2025-08-05 11:53:14 +0900

    .

--- src/color.py
+++ src/color.py
@@ -5,11 +5,14 @@ import numpy as np
 img = cv2.imread('../img/like_lenna.png')
 # bgr
 bgr = cv2.imread('../img/like_lenna.png', cv2.IMREAD_COLOR)
-
+# a
+bgra = cv2.imread('../img/like_lenna.png', cv2.IMREAD_UNCHANGED)
 # shape
-print("default", img.shape, "color", bgr.shape)
+print("default", img.shape, "color", "unchanged", bgr.shape)
+
 cv2.imshow('img', img)
 cv2.imshow('bgr', bgr)
+cv2.imshow('alphpa', bgra[:, :, 3])
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
